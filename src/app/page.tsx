daad0ae01c30d59e2ca74ff2582d3f03aa6b02b8
Ray silva de arruda <rayarruda9876@gmail.com>
2025-02-27 18:43:51 -0300

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 'use server';
-
+import Page from "./pagec.tsx"
 
 export default async function Home() {
  
   const response = await fetch('/api/notes');
     const data = await response.json();
 
-  return (<Page notes={data}>)
+  return (<Page notes={data}/>)
 }
