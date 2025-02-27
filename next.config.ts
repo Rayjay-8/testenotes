284058ac9deec2a606015f80257633db66f4e993
Ray silva de arruda <rayarruda9876@gmail.com>
2025-02-27 18:22:44 -0300

    simple

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
